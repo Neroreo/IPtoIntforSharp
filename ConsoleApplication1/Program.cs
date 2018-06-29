714facb03bfad82e9d5ad9d64ecdf4ebd0142376
kkkk <lord8333@gmail.com>
2018-06-29 23:19:15 +0900

    init commit

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net;
+
+namespace IPtoInt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string address = "255.255.255.234";
+            IPAddress ipAddr = IPAddress.Parse(address);
+
+            var ipuint32 = BitConverter.ToUInt32(IPAddress.Parse(address).GetAddressBytes(), 0);
+
+            Console.WriteLine(ipuint32);
+
+            Console.ReadLine();
+        }
+    }
+}
